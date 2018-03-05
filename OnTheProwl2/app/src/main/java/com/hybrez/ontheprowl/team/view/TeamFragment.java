67731c0a4660244540f80ff0f7d5b0891de635ad
Hamzah Aslam <hybrezz54@users.noreply.github.com>
2018-03-05 00:58:24 -0500

    Initial update for change to recycleview

--- OnTheProwl2/app/src/main/java/com/hybrez/ontheprowl/team/view/TeamFragment.java
+++ OnTheProwl2/app/src/main/java/com/hybrez/ontheprowl/team/view/TeamFragment.java
@@ -1,20 +1,44 @@
 package com.hybrez.ontheprowl.team.view;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 
 import com.hybrez.com.hybrez.ontheprowl.R;
+import com.hybrez.ontheprowl.Constants;
+import com.hybrez.ontheprowl.MainActivity;
 import com.hybrez.ontheprowl.R;
+import com.hybrez.ontheprowl.manager.ConfigManager;
+import com.hybrez.ontheprowl.manager.SharedMap;
+import com.hybrez.ontheprowl.manager.TheBlueAllianceService;
+import com.hybrez.ontheprowl.model.team.Team;
+import com.hybrez.ontheprowl.team.TeamAdapter;
 import com.hybrez.ontheprowl.team.TeamRecyclerViewAdapter;
 import com.hybrez.ontheprowl.team.dummy.DummyContent;
 import com.hybrez.ontheprowl.team.dummy.DummyContent.DummyItem;
+import com.hybrez.ontheprowl.util.io.IOUtils;
+import com.hybrez.ontheprowl.util.io.JSONUtil;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
  * A fragment representing a list of Items.
@@ -22,7 +46,7 @@ import com.hybrez.ontheprowl.team.dummy.DummyContent.DummyItem;
  * Activities containing this fragment MUST implement the {@link OnListFragmentInteractionListener}
  * interface.
  */
-public class TeamFragment extends Fragment {
+public class TeamFragment extends Fragment implements Callback<List<Team>> {
 
     // TODO: Customize parameter argument names
     private static final String ARG_COLUMN_COUNT = "column-count";
@@ -72,6 +96,22 @@ public class TeamFragment extends Fragment {
             }
             recyclerView.setAdapter(new TeamRecyclerViewAdapter(DummyContent.ITEMS, mListener));
         }
+
+        // TODO: set list view on click listener
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // get the selected team object
+                Team team = (Team) mListView.getAdapter().getItem(position);
+
+                // Launch TeamActivity with team info
+                Intent intent = new Intent(MainActivity.this, TeamActivity.class);
+                intent.putExtra(Constants.TEAM_NAME, team.getInfo().getName());
+                intent.putExtra(Constants.TEAM_NUMBER, team.getNumber());
+                startActivity(intent);
+            }
+        });
+
         return view;
     }
 
@@ -107,4 +147,83 @@ public class TeamFragment extends Fragment {
         // TODO: Update argument type and name
         void onListFragmentInteraction(DummyItem item);
     }
+
+    @Override
+    public void onResponse(Call<List<Team>> call, Response<List<Team>> response) {
+        // clear the adapter for new list
+        TeamAdapter adapter = (TeamAdapter) mListView.getAdapter();
+        adapter.clear();
+
+        // sort the list by number
+        List<Team> teams = response.body();
+        Collections.sort(teams, new Comparator<Team>() {
+            @Override
+            public int compare(Team o1, Team o2) {
+                Log.e("MainActivity", o1.getNumber() + " " + o2.getNumber());
+                Integer team1 = Integer.parseInt(o1.getNumber());
+                Integer team2 = Integer.parseInt(o2.getNumber());
+                return team1.compareTo(team2);
+            }
+        });
+
+        // add teams to list adapter
+        adapter.addAll(teams);
+        adapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onFailure(Call<List<Team>> call, Throwable t) {
+        // Show a failure message to the user
+        Snackbar.make(mListView, getString(R.string.download_failure),
+                Snackbar.LENGTH_LONG).show();
+    }
+
+    /**
+     * Check if teams are stored in data. If not,
+     * get the teams from The Blue Alliance.
+     */
+    private void getTeams() {
+        // add teams from a stored file or get from TBA
+        if (!addTeamsFromStorage()) {
+            OkHttpClient client = new OkHttpClient.Builder()
+                    .build();
+
+            Retrofit retrofit = new Retrofit.Builder()
+                    .baseUrl(Constants.TBA_BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .client(client)
+                    .build();
+
+            TheBlueAllianceService service = retrofit.create(TheBlueAllianceService.class);
+            Call<List<Team>> call = service.listTeams("2017ncgre", Constants.TBA_AUTH_KEY);
+            call.enqueue(this);
+        }
+    }
+
+    /**
+     * Add teams from a file if stored
+     * on previous use of app
+     *
+     * @return Whether or not file exists
+     */
+    private boolean addTeamsFromStorage() {
+        String file = SharedMap.getInstance(this)
+                .getTeamDataPath(ConfigManager.getEvent(this));
+        boolean fileExists = IOUtils.doesFileExist(file);
+
+        // checks whether file exists
+        if (fileExists) {
+            // parse the JSON in the file
+            List<Team> teams = JSONUtil.read(file);
+
+            // add the teams in the file to the adapter
+            TeamAdapter adapter = (TeamAdapter) mListView.getAdapter();
+            adapter.clear();
+            adapter.addAll(teams);
+            adapter.notifyDataSetChanged();
+        }
+
+        return fileExists;
+    }
+
 }
